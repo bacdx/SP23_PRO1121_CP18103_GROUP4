0c04e225725132d5cdcbba9a308cbc8e1473ef74
bacdx <bactxph20234>
2023-03-15 13:01:59 +0700

    fcomit

---
+++ app/src/main/java/com/example/sp23_pro1121_cp18103_group4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sp23_pro1121_cp18103_group4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
